f391ca9b93ef8e40618b6af066a8fc962e74d4c1
asta910 <114487898+asta910@users.noreply.github.com>
2024-09-12 22:12:15 +0530

    Update EnterName.cpp

--- EnterName.cpp
+++ EnterName.cpp
@@ -5,47 +5,37 @@ using namespace std;
 int main() {
     char name[100];
     cout << "Enter the sentence: ";
-    gets(name);  
-
-    cout << name << endl;   
+    gets(name);
 
     int vowels = 0;
     int words = 0;
     int spaces = 0;
-    int occurance = 0;
-
-    int span = strlen(name) - 1; 
+    int span = strlen(name) - 1;
+    
     int count = 0;
-
     while (span >= 0 && name[span] == ' ') {
         count++;
         span--;
     }
-    
-    int index = 0;
-    int start = 0;
 
+    int index = 0;
     while (index < strlen(name) && name[index] == ' ') {
-        start++;
         index++;
     }
 
     int len = strlen(name) - count;
     int i = 0;
 
-    while (len != 0 && i < len) {
-
+    while (i < len) {
         if (name[i] == ' ') {
             spaces++;
-
             if (i > 0 && name[i - 1] != ' ') {
                 words++;
             }
-            
         }
-        
-        if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u') vowels++;
-        
+        if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u') {
+            vowels++;
+        }
         i++;
     }
 
